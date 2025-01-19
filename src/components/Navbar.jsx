efa35190af244910679d97fcf2bf5fa26289e012
Ayon Ghosh <ayonghosh88@gmail.com>
2025-01-19 11:36:27 +0000

    Add sitar icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom'; // Import Link component from React Router
 
+import SitarIcon from './SitarIcon';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
+            <SitarIcon />
             <Link className="nav-item" to="/home">Home</Link>
             <Link className="nav-item" to="/about">About Me</Link>
             <Link className="nav-item" to="/gharana">Gharana</Link>

0d406f01b14280299103cafc6ebd78b621ca6bf8
Ayon Ghosh <ayonghosh88@gmail.com>
2025-01-21 06:42:33 +0000

    Add content for Gharana and About Me pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,17 +2,47 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 
+import "./About.css";
+
 const About = () => {
     return (
-        <div className="container">
-            <Navbar />
-            <div className="main-content">
-              <div className="text-content">
-                <h1 className="title">About Me</h1>
-                <h2 className="subtitle">Coming Soon...</h2>
-              </div>
-            </div>
+      <div className="container aboutme">
+        <Navbar />
+        <div className="aboutme-container">
+          <div className="aboutme-content text-content">
+            <h1 className="aboutme-title">About Me</h1>
+            <p className="aboutme-description">
+              Subhranil Sarkar is regarded as one of the most promising Sitar-players of the younger generation.
+              He belongs to Sitar's first family, the Etawah Gharana (also known as the Imdadkhani Gharana).
+              He was initiated into music at the age of 6 by Sri Bimal Chatterjee of the same Gharana.
+              In the year 2000, Subhranil was accepted as a disciple by the great Sitar maestro
+              Padma-Shri Ustad Shahid Parvez Khan and with this began a new chapter in Subhranil’s musical life.
+              Within a few years he has established himself as one of the foremost disciples of Ustadji
+              and one of the young torchbearers of his Gharana.
+            </p>
+            <p className="aboutme-description">
+              Subhranil started performing at the All India Radio, Kolkata at the age of 9.
+              He acquired his Masters degree in Music from the Rabindra Bharati University being the
+              first-class-first gold medalist of his year.
+              For quite a few years he was associated with the Archive of North Indian Classical Music
+              (Jadavpur University, Kolkata). He has been an Examiner as well as Adjudicator
+              with the Rabindra Bharati University, Kolkata.
+              He has presented lecture-demonstrations on the 'Sitar', ‘Etawah Gharana’ and ‘Ustad Amir Khan’
+              at a few prestigious institutions. Subhranil Sarkar has performed in numerous concerts
+              all over India, U.S. and Europe. One of the biggest highlights in Subhranil’s life
+              is to have accompanied the great Ustad Shahid Parvez Khan in concerts on the sitar.
+            </p>
           </div>
+          <div className="aboutme-image-container">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/aboutme.jpg`}
+              alt="Subhranil Sarkar with his sitar"
+              className="aboutme-image"
+            />
+            <a className="presskit" href={`${process.env.PUBLIC_URL}/assets/downloads/subhranil-sarkar-presskit.zip`}>Download Press Kit</a>
+          </div>
+        </div>
+      </div>
     );
 }
 
--- src/pages/Gharana.jsx
+++ src/pages/Gharana.jsx
@@ -2,14 +2,40 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 
+import "./Gharana.css";
+
 const Gharana = () => {
     return (
-        <div className="container">
+        <div className="container gharana">
             <Navbar />
-            <div className="main-content">
-              <div className="text-content">
-                <h1 className="title">Gharana</h1>
-                <h2 className="subtitle">Coming Soon...</h2>
+            <div className="gharana-container">
+              <div className="gharana-image-container">
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/images/gharana.png`}
+                  alt="Sitar performance of Subhranil Sarkar and Ustad Shahid Parvez Khan"
+                  className="gharana-image"
+                />
+              </div>
+              <div className="gharana-content text-content">
+                <h1 className="gharana-title">Gharana</h1>
+                <p className="gharana-description">
+                  The Etawah gharana is a North Indian school of sitar and surbahar
+                  music and named after a small town close to Agra where Imdad Khan
+                  (1848–1920) lived. It is also known as Imdadkhani gharana in the
+                  honour of its founder, Imdad Khan.
+                </p>
+                <p className="gharana-details">
+                  The gharana's achievements include the development of the Surbahar,
+                  major structural changes to both the sitar and surbahar, and the
+                  creation of the instrumental style known as the gayaki ang (vocal
+                  style performed on sitar) by Vilayat Khan. This style of sitar is now
+                  known as the Vilayatkhani sitar.
+                </p>
+                <p className="gharana-performers">
+                  Living performers of the family include Shahid Parvez, Shujaat Khan,
+                  Nishat Khan, Irshad Khan, Wajahat Khan, Hidayat Khan, and Zila Khan,
+                  the first female performer of this gharana.
+                </p>
               </div>
             </div>
           </div>
